7b1409079928eb956a9907f55cd1bc3927ce2ad4
Scorp-17 <scorpion171178@gmail.com>
2024-06-23 11:50:17 +0200

    slider edition

---
+++ script.js
@@ -0,0 +1,28 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Функция для переключения слайдов вперед или назад
+function plusSlides(n) {
+showSlides(slideIndex += n);
+}
+
+// Функция для переключения на конкретный слайд
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
